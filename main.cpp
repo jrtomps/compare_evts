5262c1c4fd90dd953fc6392a8a35f43b167f2e70
Jeromy Tompkins <tompkins@nscl.msu.edu>
2015-08-21 08:46:04 -0400

    Makes the test more generic for comparing specific items

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <RingIOV11.h>
 #include <V11/DataFormatV11.h>
 #include <V11/CRingItem.h>
+#include <V11/CRingScalerItem.h>
 #include <V11/CRingItemFactory.h>
 #include <FragmentIndex.h>
 #include <iostream>
@@ -73,6 +74,32 @@ void compareItems(CRingItem& ubItem, CRingItem bItem)
   }
 }
 
+void compareScalerItems(CRingItem& ubItem, CRingItem bItem)
+{
+  unique_ptr<CRingItem> pItem0( CRingItemFactory::createRingItem(ubItem) );
+  unique_ptr<CRingItem> pItem1( CRingItemFactory::createRingItem(bItem) );
+
+  CRingScalerItem& sclrItem0 = dynamic_cast<CRingScalerItem&>(*pItem0);
+  CRingScalerItem& sclrItem1 = dynamic_cast<CRingScalerItem&>(*pItem1);
+
+  uint16_t* pBItem  = reinterpret_cast<uint16_t*>(bItem.getItemPointer());
+  uint16_t* pUBItem = reinterpret_cast<uint16_t*>(ubItem.getItemPointer());
+
+  size_t builtSize = bItem.getBodySize();
+  size_t unbuiltSize = ubItem.getBodySize();
+
+  if ( builtSize != unbuiltSize ) {
+    cout << "index=" << setw(6) << count << " Differing sizes observed in the ring items" << endl;
+    cout << "      " << setw(6) << " " << "ubSize=" << unbuiltSize << "bSize=" << builtSize << endl;
+    printItems(cout, pUBItem, pBItem);
+  } else {
+    if ( sclrItem0.getScalers() != sclrItem1.getScalers() ) {
+      cout << "index=" << count << " Diffent content observed" << endl;
+
+      printItems(cout, pUBItem, pBItem);
+    }
+  }
+}
 
 
 bool eofCondition(std::istream& file0, std::istream& file1) 
@@ -120,11 +147,11 @@ bool errorCondition(std::istream& file0, std::istream& file1)
 
 
 
-CRingItem getNextItem(std::istream& file)
+CRingItem getNextItem(std::istream& file, uint32_t type)
 {
     CRingItem item(VOID);
 
-    while ( file && (item.type() != PHYSICS_EVENT)) {
+    while ( file && (item.type() != type)) {
       file >> item;
     }
 
@@ -144,18 +171,28 @@ int main(int argc, char* argv[])
   std::string built_file_path(argv[2]);
 
   std::ifstream file0(unbuilt_file_path.c_str());
+  if (!file0.is_open()) { cout << "failed to open " << unbuilt_file_path << endl; return 1;}
   std::ifstream file1(built_file_path.c_str());
+  if (!file1.is_open()) { cout << "failed to open " << built_file_path << endl; return 1;}
 
   count = 0;
+  uint32_t type = PHYSICS_EVENT;
   while (file0 && file1) {
-    CRingItem builtItem   = getNextItem(file0);
-    CRingItem unbuiltItem = getNextItem(file1);
+    CRingItem builtItem   = getNextItem(file0, type);
+    CRingItem unbuiltItem = getNextItem(file1, type);
 
     if ( eofCondition(file0, file1) || errorCondition(file0, file1) ) {
       break;
     }
 
-    compareItems(unbuiltItem, builtItem);
+    if (unbuiltItem.type() == PERIODIC_SCALERS && builtItem.type() == PERIODIC_SCALERS) {
+      compareScalerItems(unbuiltItem, builtItem);
+    } else if (unbuiltItem.type() == PHYSICS_EVENT && builtItem.type() == PHYSICS_EVENT) {
+      compareItems(unbuiltItem, builtItem);
+    } else {
+      cout << "Files are out of sync! Found different ring item types" << endl;
+      break;
+    }
 
     count++;
   }
